0bfc8767da60eb6b5d501353b03c062458b91737
Yakov <yaxa499@gmail.com>
2026-04-02 21:27:25 +0300

    feat: initial tg bot implementation

---
+++ bin/start_bot.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require_relative "../lib/telegram_dota_stats_bot"
+
+TelegramDotaStatsBot.run
--- lib/telegram_dota_stats_bot.rb
+++ lib/telegram_dota_stats_bot.rb
@@ -1,7 +1,29 @@
 # frozen_string_literal: true
 
+require "telegram/bot"
+require "dotenv/load"
 require_relative "telegram_dota_stats_bot/version"
 
 module TelegramDotaStatsBot
   class Error < StandardError; end
+
+  def self.run
+    token = ENV.fetch("TELEGRAM_BOT_TOKEN", nil)
+
+    puts "Ошибка: TELEGRAM_BOT_TOKEN не найден в .env файле" if token.nil?
+
+    puts "Бот запущен."
+
+    Telegram::Bot::Client.run(token) do |bot|
+      bot.listen do |message|
+        case message.text
+        when "/start"
+          bot.api.send_message(
+            chat_id: message.chat.id,
+            text: "Приветствуем тебя в DotaStats."
+          )
+        end
+      end
+    end
+  end
 end
