93fe619d0d81b0e505ec0690e6c760283796b1dd
Yakov <yaxa499@gmail.com>
2026-04-10 12:04:56 +0300

    feat: add ui for Player stats

--- lib/telegram_dota_stats_bot.rb
+++ lib/telegram_dota_stats_bot.rb
@@ -19,12 +19,48 @@ module TelegramDotaStatsBot
 
     Telegram::Bot::Client.run(token) do |bot|
       bot.listen do |message|
+        @states ||= {}
+
         case message.text
         when "/start"
           bot.api.send_message(
             chat_id: message.chat.id,
-            text: "Приветствуем тебя в DotaStats."
+            text: "Приветствуем тебя в DotaStats.",
+            reply_markup: View.main_menu
           )
+        when "👤 Посмотреть профиль игрока"
+          @states[message.from.id] = :waiting_player_id
+
+          bot.api.send_message(
+            chat_id: message.chat.id,
+            text: "Введите id игрока:"
+          )
+        when "📊 Посмотреть статистику матча"
+          @states[message.from.id] = :waiting_match_id
+
+          bot.api.send_message(
+            chat_id: message.chat.id,
+            text: "Введите id матча:"
+          )
+        else
+          case @states[message.from.id]
+          when :waiting_player_id
+            player_data = Player.new.parse_player(message.text)
+
+            puts "DEBUG: player_data is #{player_data.inspect}"
+
+            bot.api.send_message(
+              chat_id: message.chat.id,
+              text: View.render_player_stats(player_data),
+              disable_web_page_preview: true,
+              parse_mode: "HTML"
+            )
+
+            @states[message.from.id] = nil
+          when :waiting_match_id
+
+            @states[message.from.id] = nil
+          end
         end
       end
     end
--- lib/telegram_dota_stats_bot/view.rb
+++ lib/telegram_dota_stats_bot/view.rb
@@ -2,8 +2,32 @@
 
 module TelegramDotaStatsBot
   class View
-    def self.render_player(data)
-      # TODO: Форматирование текста для Телеграма
+    def self.button(text)
+      Telegram::Bot::Types::KeyboardButton.new(text: text)
+    end
+
+    def self.main_menu
+      kb = [
+        [button("👤 Посмотреть профиль игрока")],
+        [button("📊 Посмотреть статистику матча")]
+      ]
+
+      Telegram::Bot::Types::ReplyKeyboardMarkup.new(
+        keyboard: kb,
+        resize_keyboard: true,
+        one_time_keyboard: true
+      )
+    end
+
+    def self.render_player_stats(data)
+      [
+        "🔑 <b>ID:</b> <code>#{data[:id]}</code>",
+        "👤 <b>Игрок:</b> <a href='#{data[:profile_uri]}'>#{data[:name]}</a>",
+        "🏆 <b>Ранг:</b> #{data[:season_rank]}",
+        "🎮 <b>Всего игр:</b> #{data[:match_count]}",
+        "📈 <b>Винрейт:</b> #{data[:win_rate]}%",
+        "✨ <b>Dota Plus:</b> #{data[:is_dota_plus_subscriber] ? "Есть" : "Нет"}"
+      ].join("\n")
     end
   end
 end
